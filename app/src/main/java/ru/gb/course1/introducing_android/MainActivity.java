4b7f3e3b4c610ddb8b1fb3fceb8d862c0bd85b12
potapovip <samurai1487@yandex.ru>
2021-12-22 23:40:59 +0500

    Initial commit

---
+++ app/src/main/java/ru/gb/course1/introducing_android/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.gb.course1.introducing_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
